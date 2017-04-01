223ffa96679b7fbc3510139e6cbd084abea1768e
Francisco Javier Barrera Rodriguez <franciscobarrerarodriguez@users.noreply.github.com>
2017-04-01 07:29:39 -0500

    Update EventosVentantaPrincipal.java

--- src/gui/EventosVentantaPrincipal.java
+++ src/gui/EventosVentantaPrincipal.java
@@ -32,7 +32,8 @@ public class EventosVentantaPrincipal implements ActionListener {
 			this.ventantaPrincipal.getThread().start();
 		}
 		if (e.getSource().equals(this.ventantaPrincipal.jButtonBloquear)) {
-			this.ventantaPrincipal.getProcesos().getBloqueo().anadirBloqueo(this.ventantaPrincipal.getProcesos().getEjecucion().getProceso());		}
+			this.ventantaPrincipal.getProcesos().getBloqueo().anadirBloqueo(this.ventantaPrincipal.getProcesos().getEjecucion().getProceso());		
+		}
 	}
 
 }
